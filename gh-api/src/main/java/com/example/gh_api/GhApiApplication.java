2040230958ecab82b454b059a0f89960f223e9ca
Lorran Henrique Rezende Alves <72421034+lorranhenrique@users.noreply.github.com>
2025-04-15 08:04:34 -0300

    Initial Commit

---
+++ gh-api/src/main/java/com/example/gh_api/GhApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gh_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhApiApplication.class, args);
+	}
+
+}
