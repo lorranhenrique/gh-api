2e70390ea552dbed4a4117995de2837f3c8a9160
mateus mauricio <mateusmau@gmail.com>
2025-05-20 08:36:05 -0300

    controllers
    alguns controllers

---
+++ src/main/java/com/example/gh_api/api/dto/controller/CargoController.java
@@ -0,0 +1,32 @@
+package com.example.gh_api.api.dto.controller;
+
+import com.example.gh_api.api.dto.CargoDTO;
+
+import com.example.gh_api.exception.RegraNegocioException;
+import com.example.gh_api.model.entity.Cargo;
+import com.example.gh_api.service.CargoService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/api/v1/cargos")
+@RequiredArgsConstructor
+@CrossOrigin
+
+public class CargoController {
+
+    private final CargoService service;
+
+    public Cargo convert(CargoDTO dto){
+        ModelMapper modelMapper = new ModelMapper();
+        Cargo cargo = modelMapper.map(dto, Cargo.class);
+        return cargo;
+    }
+}
